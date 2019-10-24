316de1bf509639b2dfe416ea0251fd56cd7ae3d2
Paweł Tomczyk <pawel.tomczyk@absysco.pl>
2019-10-24 20:47:06 +0200

    2046

--- Program.cs
+++ Program.cs
@@ -10,7 +10,65 @@ namespace LRN_CS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Dupa");
+            bool continueWordUnscramble = true;
+            do
+            {
+                Console.WriteLine("Please enter the option - F for File and M for Manual");
+                var option = Console.ReadLine() ?? string.Empty;
+
+                switch (option.ToUpper())
+                {
+                    case "F":
+                        {
+                            Console.Write("Enter scrambled words file name: ");
+                            ExecuteScrambledWordsInFileScenario();
+                            break;
+                        }
+                    case "M":
+                        {
+                            Console.Write("Enter scrambled words manually");
+                            ExecuteScrambledWordsManualEntryScenario();
+                            break;
+                        }
+                    default:
+                        {
+                            Console.Write("Option was not recognized.");
+                            break;
+                        }
+                }
+                var continueDecision = string.Empty;
+                do
+                {
+                    Console.WriteLine("Do you want to continue? Y/N");
+                    continueDecision = (Console.ReadLine() ?? String.Empty);
+                } while (!continueDecision.Equals("Y", StringComparison.OrdinalIgnoreCase) && 
+                        !continueDecision.Equals("N", StringComparison.OrdinalIgnoreCase));
+
+                continueWordUnscramble = continueDecision.Equals("Y", StringComparison.OrdinalIgnoreCase);
+
+            } while (continueWordUnscramble);
+
         }
+
+        private static void ExecuteScrambledWordsManualEntryScenario()
+        {
+            var manualInput = Console.ReadLine() ?? string.Empty;
+            string[] scrambledWords = manualInput.Split(',');
+            DisplayMatchedUnscrambledWords();
+        }
+
+        private static void ExecuteScrambledWordsInFileScenario()
+        {
+            var fileName = Console.ReadLine() ?? string.Empty;
+            string[] scrambledWords = _fileReader.Read(fileName);
+        }
+        private static void DisplayMatchedUnscrambledWords(string[] scrambledWords)
+        {
+            string[] wordList = _fileReader.Read(wordListFileName);
+
+            List<MatchedWord> matchedWords = _wordMatcher.Match(scrambledWords, wordList);
+        }
+
+
     }
 }
